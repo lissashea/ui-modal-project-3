f6b459aeee1dab8dc784d645b093b5ebc16ac88f
“Lissa <lwarshaw674@gmail.com>
2023-05-05 09:02:43 -0400

    updated api config

---
+++ src/utils/apiconfig.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+let apiUrl;
+
+const apiUrls = {
+  production: "https://ancient-coast-33215.herokuapp.com/football/",
+  development: "http://127.0.0.1:3000/"
+};
+
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
+} else {
+  apiUrl = apiUrls.production;
+}
+
+const api = axios.create({
+  baseURL: apiUrl,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+export default api;
